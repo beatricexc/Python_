f46f189ecf273c12acb011b826c2af9ca837b115
beatricx <72341578+beatricexc@users.noreply.github.com>
2022-09-28 21:55:27 +0100

    Add files via upload

--- vsearch.py
+++ vsearch.py
@@ -1,8 +1,11 @@
-def search4vowels(phrase:str)-> set:
-    """Display any vowels found in an asked-for word. """
+
+
+def search4vowels(phrase: str) -> set:
+    """ Display any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str, letters:str = 'aeiou') -> set:
-    """Return a set of the 'letters' argument found in 'phrase'."""
+
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
+    """Returns a set of the 'letters' found in'phrase'."""
     return set(letters).intersection(set(phrase))
